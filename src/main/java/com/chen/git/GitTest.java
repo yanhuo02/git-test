1a4bb866ecb4b54958b2719c5f9f5c610f3d62fe
Chen <1743140981@qq.com>
2022-11-18 13:06:16 +0800

    second commit

--- src/main/java/com/chen/git/GitTest.java
+++ src/main/java/com/chen/git/GitTest.java
@@ -3,5 +3,6 @@ package com.chen.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
